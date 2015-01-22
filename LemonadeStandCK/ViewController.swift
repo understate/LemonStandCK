436e1dabc82daffed3c9658f6c723a11dd957ca9
understate <c-koehler@web.de>
2015-01-22 18:00:06 +0100

    5th upload - added function for purchase buttons

--- LemonadeStandCK/ViewController.swift
+++ LemonadeStandCK/ViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    
+    //Label Declaration . Main Storyboard
     @IBOutlet weak var currentEuroLabel: UILabel!
     
     @IBOutlet weak var currentLemonLabel: UILabel!
@@ -26,6 +26,22 @@ class ViewController: UIViewController {
     @IBOutlet weak var mixIceCubesLabel: UILabel!
     
     
+    // Stats Label Varibales
+    var currentEuro = 10
+    var currentLemons:Int = 1
+    var currentIceCubes:Int = 1
+
+    
+    
+    // Purchase Variables
+    var purchaseLemons:Int = 0
+    var purchaseIceCubes:Int = 0
+    
+    
+    // Mix Variables
+    var mixLemons:Int = 0
+    var mixIceCubes:Int  = 0
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -37,33 +53,91 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-    @IBAction func purchaseLemonPlusButtonPressed(sender: UIButton) {
+    @IBAction func purchaseLemonPlusButtonPressed(sender: AnyObject) {
+        if currentEuro < 2 {
+            println("Not enough budget")
+            }
+        else {
+        ++purchaseLemons
+        purchaseLemonLabel.text = "\(purchaseLemons)"
+        ++currentLemons
+        currentLemonLabel.text = "\(currentLemons) Lemons"
+        currentEuro = currentEuro - 2
+        currentEuroLabel.text = "\(currentEuro) €"
+        }
+        
     }
     
     
-    @IBAction func purchaseLemonMinusButtonPressed(sender: UIButton) {
+    @IBAction func purchaseLemonMinusButtonPressed(sender: AnyObject) {
+        if currentLemons > 0 {
+        --purchaseLemons
+        purchaseLemonLabel.text = "\(purchaseLemons)"
+        --currentLemons
+        currentLemonLabel.text = "\(currentLemons) Lemons"
+        currentEuro = currentEuro + 2
+        currentEuroLabel.text = "\(currentEuro) €"
+        }
+        else {
+            println("No lemon to sell")
+        }
+        
     }
     
-    @IBAction func purchaseIceCubesPlusButtonPressed(sender: UIButton) {
+    @IBAction func purchaseIceCubesPlusButtonPressed(sender: AnyObject) {
+        if currentEuro > 0 {
+            ++currentIceCubes
+            ++purchaseIceCubes
+            --currentEuro
+            currentIceCubesLabel.text = "\(currentIceCubes) Ice Cubes"
+            purchaseIceCubesLabel.text = "\(purchaseIceCubes)"
+            currentEuroLabel.text = "\(currentEuro) €"
+        }
+        else {
+            println ("Not enough budget")
+        }
     }
     
-    @IBAction func purchaseIceCubesMinusButtonPressed(sender: UIButton) {
+    
+    
+    @IBAction func purchaseIceCubesMinusButtonPressed(sender: AnyObject) {
+        if currentIceCubes > 0 {
+            --currentIceCubes
+            --purchaseIceCubes
+            ++currentEuro
+            currentIceCubesLabel.text = "\(currentIceCubes) Ice Cubes"
+            purchaseIceCubesLabel.text = "\(purchaseIceCubes)"
+            currentEuroLabel.text = "\(currentEuro) €"
+            
+        }
+        else {
+            println("no more Ice Cubes")
+        }
     }
     
-    @IBAction func mixLemonPlusButtonPressed(sender: UIButton) {
+    
+    
+    @IBAction func mixLemonPlusButtonPressed(sender: AnyObject) {
     }
   
-    @IBAction func mixLemonMinusButtonPressed(sender: UIButton) {
+    
+    
+    @IBAction func mixLemonMinusButtonPressed(sender: AnyObject) {
     }
     
     
-    @IBAction func mixIceCubesPlusButtonPressed(sender: UIButton) {
+    
+    @IBAction func mixIceCubesPlusButtonPressed(sender: AnyObject) {
     }
     
-    @IBAction func mixIceCubesMinusButtonPressed(sender: UIButton) {
+    
+    
+    @IBAction func mixIceCubesMinusButtonPressed(sender: AnyObject) {
     }
     
-    @IBAction func startDayButtonPressed(sender: UIButton) {
+    
+    
+    @IBAction func startDayButtonPressed(sender: AnyObject) {
     }
 
 }
